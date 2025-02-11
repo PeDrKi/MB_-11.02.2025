dbcda29420751fb04590e3e05aefb42dc9ebece5
PeDrKi <pdkhue2004@gmail.com>
2025-02-11 09:34:53 +0700

    Submit Project _ PDK

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [bgColor, setBgColor] = useState("#FFFFFF");
+
+  const colors = [
+    { name: "GREEN", color: "green" },
+    { name: "BLUE", color: "blue" },
+    { name: "BROWN", color: "brown" },
+    { name: "YELLOW", color: "yellow" },
+    { name: "RED", color: "red" },
+    { name: "BLACK", color: "black" },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: bgColor }]}> 
+      {colors.map((item, index) => (
+        <TouchableOpacity
+          key={index}
+          style={[styles.button, { backgroundColor: item.color }]}
+          onPress={() => setBgColor(item.color)}
+        >
+          <Text style={styles.buttonText}>{item.name}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    width: "80%",
+    padding: 15,
+    marginVertical: 10,
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,19 @@
+# Họ và Tên: Phạm Đăng Khuê
+## MSV      : 22810310270
+---
+### Sử dụng State
+---
+#### Main
+![Main](./Submit/SC1.jpg)
+#### Green
+![Green](./Submit/SC2.jpg)
+#### Blue
+![Blue](./Submit/SC3.jpg)
+#### Brown
+![Brown](./Submit/SC4.jpg)
+#### Yellow
+![Yellow](./Submit/SC5.jpg)
+#### Red
+![Red](./Submit/SC6.jpg)
+#### Black
+![Black](./Submit/SC7.jpg)
